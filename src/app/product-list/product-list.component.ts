9833456bd88debfee3349755625882b96b05708a
unozer <40232537+unozer@users.noreply.github.com>
2025-04-16 16:53:25 +0200

    event binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,13 +10,11 @@ import { Product } from '../product';
 })
 export class ProductListComponent {
   products: Product[] = [
-
+    { id: 1, title: 'Keyboard'},
+    { id: 2, title: 'Microphone'},
+    { id: 3, title: 'Webcam'},
+    { id: 4, title: 'Tablet'}
   ];
 
-  isLiked: boolean = true;
-
-  currentStyles = {
-    color: 'red',
-    with: '100px'
-  };
+  selectedProduct: Product | undefined;
 }
