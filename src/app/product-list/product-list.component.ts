c923cb78fb33c8041a8d6036490eeaa471b3c5a3
unozer <40232537+unozer@users.noreply.github.com>
2025-04-28 15:53:18 +0200

    service DI

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { SortPipe } from '../pipes/sort.pipe';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product-list',
@@ -10,17 +11,22 @@ import { SortPipe } from '../pipes/sort.pipe';
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
-export class ProductListComponent {
-  products: Product[] = [
-    { id: 1, title: 'Keyboard', price: 100, categories: { 1: 'Computing', 2: 'Peripherals' } },
-    { id: 2, title: 'Microphone', price: 35, categories: { 3: 'Multimedia' } },
-    { id: 3, title: 'Webcam', price: 79, categories: { 1: 'Computing', 3: 'Multimedia' } },
-    { id: 4, title: 'Tablet', price: 500, categories: { 4: 'Entertainment' } },
-  ];
+export class ProductListComponent implements OnInit {
+
+  private productsService = inject(ProductsService);
+
+  products: Product[] = [];
 
   selectedProduct: Product | undefined = this.products[0];
 
   onAdded(product: Product) {
     alert(`${product.title} Product added to cart!`)
+  }
+
+  ngOnInit(): void {
+    this.products = this.productsService.getProducts();
+  }
+
+  constructor() {
   }
 }
