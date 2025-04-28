dd5f51665857db1a816234cba8d115d19ac17d31
unozer <40232537+unozer@users.noreply.github.com>
2025-04-28 17:49:28 +0200

    provider value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CopyrightDirective } from './directives/copyright.directive';
+import { APP_SETTINGS, appSettings } from './app.settings';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, ProductListComponent, CopyrightDirective],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [
+    { provide: APP_SETTINGS, useValue: appSettings },
+  ],
 })
 export class AppComponent {
   title = 'shopping';
+
+  settings = inject(APP_SETTINGS);
 }
