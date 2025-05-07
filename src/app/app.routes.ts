2d529a31dd77b9a6c5f3b83cdef5a555d523caad
unozer <40232537+unozer@users.noreply.github.com>
2025-05-07 15:33:54 +0200

    route new product

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CartComponent } from './cart/cart.component';
+import { ProductCreateComponent } from './product-create/product-create.component';
 
 export const routes: Routes = [
+    { path: 'products/new', component: ProductCreateComponent },
     { path: 'products', component: ProductListComponent },
-    { path: 'cart', component: CartComponent }
+    { path: 'cart', component: CartComponent },
 ];
