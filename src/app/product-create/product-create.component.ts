91893791b1f956faab10f05d569d0f4b117644c7
unozer <40232537+unozer@users.noreply.github.com>
2025-05-08 15:36:43 +0200

    reactive form

--- src/app/product-create/product-create.component.ts
+++ src/app/product-create/product-create.component.ts
@@ -1,22 +1,30 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product-create',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.scss'
 })
 export class ProductCreateComponent {
+
+  productForm = new FormGroup({
+    title: new FormControl('', { nonNullable: true }),
+    price: new FormControl<number | undefined>(undefined, { nonNullable: true }),
+    category: new FormControl('', { nonNullable: true })
+  });
+
   constructor(private productsService: ProductsService, private router: Router) {}
 
-  createProduct(title: string, price: string, category: string) {
+  createProduct() {
     this.productsService.addProduct({ 
-      title, 
-      price: Number(price), 
-      category 
+      title: this.productForm.controls.title.value, 
+      price: this.productForm.controls.price.value,
+      category: this.productForm.controls.category.value 
     }).subscribe(() => this.router.navigate(['/products']));
   }
 }
