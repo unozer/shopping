6228af1a053c7d58c9fb57cd4ce068d61943ef07
unozer <40232537+unozer@users.noreply.github.com>
2025-05-12 17:30:48 +0200

    material card

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,29 +6,25 @@ import { Product } from '../product';
 import { SortPipe } from '../pipes/sort.pipe';
 import { MatMiniFabButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [SortPipe, RouterLink, MatMiniFabButton, MatIcon],
+  imports: [SortPipe, RouterLink, MatMiniFabButton, MatIcon, MatCardModule],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrl: './product-list.component.scss',
 })
 export class ProductListComponent {
-
   products = toSignal(
-    this.route.data.pipe(
-      switchMap(data => of(data['products']))
-    )
+    this.route.data.pipe(switchMap((data) => of(data['products'])))
   );
 
   selectedProduct: Product | undefined;
 
   onAdded(product: Product) {
-    alert(`${product.title} Product added to cart!`)
+    alert(`${product.title} Product added to cart!`);
   }
 
-  constructor(
-    private route: ActivatedRoute
-  ) {}
+  constructor(private route: ActivatedRoute) {}
 }
