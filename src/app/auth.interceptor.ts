b465bf908ed8281574d5cca720327b75a97fe53a
unozer <40232537+unozer@users.noreply.github.com>
2025-05-12 15:34:37 +0200

    interceptor 401 error

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,11 +1,21 @@
 import { inject } from '@angular/core';
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn, HttpStatusCode } from '@angular/common/http';
 import { AuthService } from './auth.service';
+import { catchError, EMPTY, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const authReq = req.clone({
     setHeaders: { Authorization: 'myToken' },
   });
-  return next(authReq);
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === HttpStatusCode.Unauthorized) {
+        authService.logout();
+        return EMPTY;
+      } else {
+        return throwError(() => error);
+      }
+    })
+  );
 };
