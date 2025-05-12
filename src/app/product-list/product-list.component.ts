f53f31c594841a518d60b902370e7a4bff7b0a48
unozer <40232537+unozer@users.noreply.github.com>
2025-05-12 17:44:46 +0200

    material table

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,16 +2,26 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import { Component } from '@angular/core';
 import { RouterLink, ActivatedRoute } from '@angular/router';
 import { of, switchMap } from 'rxjs';
+import { CurrencyPipe } from '@angular/common';
 import { Product } from '../product';
 import { SortPipe } from '../pipes/sort.pipe';
 import { MatMiniFabButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [SortPipe, RouterLink, MatMiniFabButton, MatIcon, MatCardModule],
+  imports: [
+    SortPipe,
+    RouterLink,
+    MatMiniFabButton,
+    MatIcon,
+    MatCardModule,
+    MatTableModule,
+    CurrencyPipe,
+  ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
 })
@@ -21,6 +31,8 @@ export class ProductListComponent {
   );
 
   selectedProduct: Product | undefined;
+
+  columnNames = ['title', 'price'];
 
   onAdded(product: Product) {
     alert(`${product.title} Product added to cart!`);
