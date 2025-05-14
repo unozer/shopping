2e8b06d0e9027d2b7e27e49c6e622093eeab71ba
unozer <40232537+unozer@users.noreply.github.com>
2025-05-14 16:53:10 +0200

    withFetch

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, ErrorHandler } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { APP_SETTINGS, appSettings } from './app.settings';
 
 import { routes } from './app.routes';
@@ -11,9 +11,11 @@ import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withComponentInputBinding()), 
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withInterceptors([authInterceptor]), withFetch()),
     { provide: APP_SETTINGS, useValue: appSettings },
-    { provide: ErrorHandler, useClass: AppErrorHandler }, provideAnimationsAsync(), provideClientHydration(),
+    { provide: ErrorHandler, useClass: AppErrorHandler },
+    provideAnimationsAsync(),
+    provideClientHydration(),
   ],
 };
