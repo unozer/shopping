73fb3f3222cc8244c6dd45e2effd6191cc0c9bcf
unozer <stra.perrino@gmail.com>
2025-05-20 11:53:18 +0200

    mock dip test checkout-component

--- src/app/checkout/checkout.component.spec.ts
+++ src/app/checkout/checkout.component.spec.ts
@@ -1,17 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CheckoutComponent } from './checkout.component';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 describe('CheckoutComponent', () => {
   let component: CheckoutComponent;
   let fixture: ComponentFixture<CheckoutComponent>;
+  const mockDialogData = { total: 100, items: [] };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CheckoutComponent]
+      imports: [CheckoutComponent],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: mockDialogData }
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(CheckoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
