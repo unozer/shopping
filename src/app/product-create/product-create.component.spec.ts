7b7209fc29d2ae5b79838a90441e1246bed0eb99
unozer <stra.perrino@gmail.com>
2025-05-20 12:04:38 +0200

    mock dip test - product-create-component

--- src/app/product-create/product-create.component.spec.ts
+++ src/app/product-create/product-create.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductCreateComponent } from './product-create.component';
+import { provideRouter } from '@angular/router';
+import { routes } from '../app.routes';
+import { ProductsService } from '../products.service';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ProductCreateComponent', () => {
   let component: ProductCreateComponent;
   let fixture: ComponentFixture<ProductCreateComponent>;
+  let productsServiceStub: Partial<ProductsService>;
 
   beforeEach(async () => {
+    productsServiceStub = {};
+
     await TestBed.configureTestingModule({
-      imports: [ProductCreateComponent]
+      imports: [ProductCreateComponent, NoopAnimationsModule],
+      providers: [
+        { provide: ProductsService, useValue: productsServiceStub },
+        provideRouter(routes)
+      ]
     })
     .compileComponents();
     
