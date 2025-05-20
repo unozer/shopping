dae56f7193d82299a712e8a9bf06e569e08b7b4d
unozer <stra.perrino@gmail.com>
2025-05-20 11:28:52 +0200

    mock dipendenze nel test app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,34 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CartService } from './cart.service';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+import { AuthService } from './auth.service';
+import { APP_SETTINGS, appSettings } from './app.settings';
 
 describe('AppComponent', () => {
+
+  let cartServiceStub: Partial<CartService>;
+  let authServiceStub: Partial<AuthService>;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+
   beforeEach(async () => {
+    cartServiceStub = {};
+    authServiceStub = {};
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post']);
+    httpClientSpy.get.and.returnValue(of({}));
+    httpClientSpy.post.and.returnValue(of({}));
+
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        { provide: HttpClient, useValue: httpClientSpy },
+        { provide: CartService, useValue: cartServiceStub },
+        { provide: APP_SETTINGS, useValue: appSettings },
+        provideRouter(routes)
+      ],
     }).compileComponents();
   });
 
