7bdef841246e605daba2e6c02a08163855d6584c
unozer <stra.perrino@gmail.com>
2025-05-20 11:35:46 +0200

    mock dipendenze per test auth component

--- src/app/auth/auth.component.spec.ts
+++ src/app/auth/auth.component.spec.ts
@@ -1,17 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthComponent } from './auth.component';
+import { AuthService } from '../auth.service';
+import { computed } from '@angular/core';
 
 describe('AuthComponent', () => {
   let component: AuthComponent;
   let fixture: ComponentFixture<AuthComponent>;
+  let authServiceStub: Partial<AuthService>;
 
   beforeEach(async () => {
+    authServiceStub = {
+      isLoggedIn: computed(() => false)
+    };
+
     await TestBed.configureTestingModule({
-      imports: [AuthComponent]
+      imports: [AuthComponent],
+      providers: [
+        { provide: AuthService, useValue: authServiceStub }
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(AuthComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
