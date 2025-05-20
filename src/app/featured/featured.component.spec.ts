794969512299941d0fe81c94dc2cd923a4e763d2
unozer <stra.perrino@gmail.com>
2025-05-20 11:56:28 +0200

    mock dip test - featured-component

--- src/app/featured/featured.component.spec.ts
+++ src/app/featured/featured.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FeaturedComponent } from './featured.component';
+import { ProductsService } from '../products.service';
+import { of } from 'rxjs';
 
 describe('FeaturedComponent', () => {
   let component: FeaturedComponent;
   let fixture: ComponentFixture<FeaturedComponent>;
+  let productsServiceStub: Partial<ProductsService>;
 
   beforeEach(async () => {
+    productsServiceStub = {
+      getFeatured: () => of({ id: 1, title: '', price: 0, category: '', image: '' })
+    };
+
     await TestBed.configureTestingModule({
-      imports: [FeaturedComponent]
+      imports: [FeaturedComponent],
+      providers: [
+        { provide: ProductsService, useValue: productsServiceStub }
+      ] 
     })
     .compileComponents();
     
