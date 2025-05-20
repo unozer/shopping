42e9a615a19791350d5c38131f4afdf20e9f18e0
unozer <stra.perrino@gmail.com>
2025-05-20 14:27:23 +0200

    mock dip test - product-service

--- src/app/products.service.spec.ts
+++ src/app/products.service.spec.ts
@@ -1,13 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProductsService } from './products.service';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { APP_SETTINGS, appSettings } from './app.settings';
 
 describe('ProductsService', () => {
   let service: ProductsService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        { provide: APP_SETTINGS, useValue: appSettings },
+      ]
+    });
     service = TestBed.inject(ProductsService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
