833a6d52d720b33b00ab861cc86ae896b2f5421e
unozer <stra.perrino@gmail.com>
2025-05-20 14:33:19 +0200

    mock dip test - cart-service

--- src/app/cart.service.spec.ts
+++ src/app/cart.service.spec.ts
@@ -1,13 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CartService } from './cart.service';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { APP_SETTINGS, appSettings } from './app.settings';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('CartService', () => {
   let service: CartService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        { provide: APP_SETTINGS, useValue: appSettings },
+      ]
+    });
     service = TestBed.inject(CartService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
