c3b5327f53488308b7961330f20d73dacf87d153
unozer <stra.perrino@gmail.com>
2025-05-20 14:30:22 +0200

    mock dip test - auth-service

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -1,13 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { APP_SETTINGS, appSettings } from './app.settings';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let httpTestingController: HttpTestingController;
+
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        { provide: APP_SETTINGS, useValue: appSettings },
+      ]
+    });
     service = TestBed.inject(AuthService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
