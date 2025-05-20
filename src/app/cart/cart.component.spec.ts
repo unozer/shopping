aed9f2d9a95cdcd246a6b1b3470ae5df2ad26767
unozer <stra.perrino@gmail.com>
2025-05-20 11:45:11 +0200

    mock dip test cart-component

--- src/app/cart/cart.component.spec.ts
+++ src/app/cart/cart.component.spec.ts
@@ -1,17 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CartComponent } from './cart.component';
+import { CartService } from '../cart.service';
+import { ProductsService } from '../products.service';
+import { of } from 'rxjs';
 
 describe('CartComponent', () => {
   let component: CartComponent;
   let fixture: ComponentFixture<CartComponent>;
+  let cartServiceStub: Partial<CartService>;
+  let productsServiceStub: Partial<ProductsService>;
 
   beforeEach(async () => {
+    cartServiceStub = {};
+    productsServiceStub = {
+      getProducts: jasmine.createSpy('getProducts').and.returnValue(of([]))
+    };
+
     await TestBed.configureTestingModule({
-      imports: [CartComponent]
+      imports: [CartComponent],
+      providers: [
+        { provide: CartService, useValue: cartServiceStub },
+        { provide: ProductsService, useValue: productsServiceStub }
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(CartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
