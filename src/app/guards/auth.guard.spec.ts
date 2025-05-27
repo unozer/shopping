aad94fca9301f1a6293a77a675d6d55c0c480766
unozer <stra.perrino@gmail.com>
2025-05-27 12:52:39 +0200

    fix import

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
-import { CanActivateFn, UrlTree } from '@angular/router';
+import { UrlTree } from '@angular/router';
 import { authGuard } from './auth.guard';
 import { AuthService } from '../services/auth.service';
 
