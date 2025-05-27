3527b501fb935fa7f4cdf0f8ac319d23ef9607e1
unozer <stra.perrino@gmail.com>
2025-05-27 12:51:04 +0200

    test specs auth-guard

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,17 +1,50 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
-
+import { Router } from '@angular/router';
+import { CanActivateFn, UrlTree } from '@angular/router';
 import { authGuard } from './auth.guard';
+import { AuthService } from '../services/auth.service';
 
-// describe('authGuard', () => {
-//   const executeGuard: CanActivateFn = (...guardParameters) => 
-//       TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+describe('authGuard', () => {
+    let authServiceSpy: jasmine.SpyObj<AuthService>;
+    let routerSpy: jasmine.SpyObj<Router>;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//   });
+    const mockRoute: any = {};
+    const mockState: any = {};
 
-//   it('should be created', () => {
-//     expect(executeGuard).toBeTruthy();
-//   });
-// });
+    const executeGuard = () =>
+        TestBed.runInInjectionContext(() => authGuard(mockRoute, mockState));
+
+    beforeEach(() => {
+        authServiceSpy = jasmine.createSpyObj('AuthService', ['isLoggedIn']);
+        routerSpy = jasmine.createSpyObj('Router', ['parseUrl']);
+
+        TestBed.configureTestingModule({
+            providers: [
+                { provide: AuthService, useValue: authServiceSpy },
+                { provide: Router, useValue: routerSpy }
+            ]
+        });
+    });
+
+    it('should be created', () => {
+        expect(executeGuard).toBeDefined();
+    });
+
+    it('should return true if user is logged in', () => {
+        authServiceSpy.isLoggedIn.and.returnValue(true);
+        const result = executeGuard();
+        expect(result).toBeTrue();
+        expect(authServiceSpy.isLoggedIn).toHaveBeenCalled();
+    });
+
+    it('should redirect to "/" if user is not logged in', () => {
+        authServiceSpy.isLoggedIn.and.returnValue(false);
+        const fakeUrlTree = {} as UrlTree;
+        routerSpy.parseUrl.and.returnValue(fakeUrlTree);
+
+        const result = executeGuard();
+        expect(result).toBe(fakeUrlTree);
+        expect(authServiceSpy.isLoggedIn).toHaveBeenCalled();
+        expect(routerSpy.parseUrl).toHaveBeenCalledWith('/');
+    });
+});
